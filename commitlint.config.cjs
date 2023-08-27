4d2982566ae6be4211e4528248c44b2d4c74b402
SonderMX <sonder-mx@outlook.com>
2023-08-27 15:57:26 +0800

    chore: 项目基本配置

---
+++ commitlint.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  // 校验规则
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 新功能（feature）
+        'fix', // 修补bug
+        'docs', // 文档（documentation）
+        'style', // 格式（不影响代码运行的变动）
+        'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回滚
+        'build',
+      ],
+    ],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72],
+  },
+}
