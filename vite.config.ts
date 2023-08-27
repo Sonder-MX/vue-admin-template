a8a26b754aa3b418077acc224cbf4c247b690ec1
SonderMX <sonder-mx@outlook.com>
2023-08-27 20:48:19 +0800

    feat: mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,40 @@
 import { defineConfig } from 'vite'
+import { viteMockServe } from 'vite-plugin-mock'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // 引入svg插件
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  server: {
-    // open: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      // 引入svg插件
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      // mock插件
+      viteMockServe({
+        localEnabled: command === 'serve',
+      }),
+    ],
+    server: {
+      // open: true,
     },
-  },
-  // 配置全局scss变量
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";',
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
       },
     },
-  },
+    // 配置全局scss变量
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";',
+        },
+      },
+    },
+  }
 })
